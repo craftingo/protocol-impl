9b1016f622e834089a0b9e283e18da6d45f61986
Lukas Schulte Pelkum <lukaesebrot@gmail.com>
2020-03-30 15:53:04 +0200

    Implement basic connection structure

---
+++ connection.go
@@ -0,0 +1,65 @@
+package protocol_impl
+
+import (
+	"bufio"
+	"io"
+	"net"
+)
+
+// Connection represents a Minecraft connection
+type Connection struct {
+	Socket net.Conn
+	io.ByteReader
+	io.Writer
+	threshold int
+}
+
+// Listener represents a connection listener
+type Listener struct {
+	net.Listener
+}
+
+// Listen listens for new Minecraft connections
+func Listen(address string) (*Listener, error) {
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Listener{listener}, nil
+}
+
+// Accept accepts a new Minecraft connection
+func (listener Listener) Accept() (Connection, error) {
+	connection, err := listener.Listener.Accept()
+	return Connection{
+		Socket:     connection,
+		ByteReader: bufio.NewReader(connection),
+		Writer:     connection,
+	}, err
+}
+
+// SetThreshold sets the threshold for a Minecraft connection
+func (connection *Connection) SetThreshold(threshold int) {
+	connection.threshold = threshold
+}
+
+// ReadPacket reads a packet from a Minecraft connection
+func (connection *Connection) ReadPacket() (Packet, error) {
+	packet, err := ReadPacket(connection.ByteReader, connection.threshold > 0)
+	if err != nil {
+		return Packet{}, err
+	}
+	return *packet, nil
+}
+
+// WritePacket writes a packet to to a Minecraft connection
+func (connection *Connection) WritePacket(packet Packet) error {
+	_, err := connection.Write(packet.Pack(connection.threshold))
+	return err
+}
+
+// Close closes a Minecraft connection
+func (connection Connection) Close() error {
+	return connection.Socket.Close()
+}
